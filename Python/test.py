b149a9146e7d10c423e00cf3095eabe935795460
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-07-27 03:50:22 +0900

    Algorithm md 파일 추가

---
+++ Python/test.py
@@ -0,0 +1,14 @@
+from os import sep
+import sys
+import time
+start = time.time()
+
+
+# A와 B를 입력받는다
+# 첫 째 줄에 A와 B 1의 자리 곱한것을 출력한다
+    # B 1의 자리 구하는 방법 : 100으로 나눈 나머지
+
+result = map(int, ['231', '456'])
+print(result)
+
+print("time : ", time.time() - start)
