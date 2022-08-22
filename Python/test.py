49969c1085fb5a3a59add66acb5ab761f82a43c7
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-08-22 23:49:46 +0900

    Algorithm Day13 Upload

--- Python/test.py
+++ Python/test.py
@@ -1,62 +1,21 @@
+height_list = []
+one = 0
+two = 0
+is_break = False
 
-# 1. 같은 눈 세개 : 10,000 + 같은 눈 * 1,000
-# 2. 같은 눈 두개 : 1,000 + 같은 눈 * 100
-# 3. 모두 다른 눈 : 그 중 가장 큰 눈 * 100
+for _ in range(9):
+    height_list.append(int(input()))
 
-# a, b, c = map(int, input().split())
-
-# if a == b == c: print(10000 + a * 1000)
-# else:
-#     if a==b or a==c: print(1000 + a * 100)
-#     elif b==c: print(1000 + b * 100)
-#     else: 
-#         list = [a,b,c]
-#         print(max(list)*100)
-
-# lst = sorted(list(map(int, input().split())))
-
-# if len(set(lst)) is 1:
-#     print(10000 + lst[0]*1000)
-# elif len(set(lst)) is 2:
-#     print(1000 + lst[1]*100)  # pick middle
-# else:
-#     print(lst[2]*100)
-
-# 생성자 없는 수 구하기
-# a = [x for x in range(0,10001)]
-# for i in range(0,10001):
-#     result = i
-#     for j in str(i):
-#         result += int(j)
-#     if result in a:
-#         a.remove(result)
-# print(*a, sep="\n")
-
-#한수 개수 구하기
-# a = input()
-# result = []
-# for i in range(97, 123):
-#     if chr(i) in a:
-#         result.append(a.index(chr(i)))
-#     else:
-#         result.append(-1)
-# print(*result, sep=" ")
-
-
-def answer(n, i):
-    if n==1:
-        print(f'{"_"*4*i}\"재귀함수가 뭔가요?\"')
-        print(f'{"_"*4*i}\"재귀함수는 자기 자신을 호출하는 함수라네\"')
-    else:
-        print(f'{"_"*4*i}\"재귀함수가 뭔가요?\"')
-        print(f'{"_"*4*i}\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n{"_"*4*i}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n{"_"*4*i}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"')     
-        answer(n-1, i+1)
-    print(f'{"_"*4*i}라고 답변하였지.')
-
-n = int(input())
-print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-print("\"재귀함수가 뭔가요?\"")
-print("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
-i = 1
-answer(n, i)
-print("라고 답변하였지.")
+for i in range(0, 8):
+    for j in range(i+1, 9):
+        if height_list[i] + height_list[j] == sum(height_list) - 100:
+            one = height_list[i]
+            two = height_list[j]
+            is_break = True
+            break
+    if is_break:
+        break
+height_list.remove(one)
+height_list.remove(two)
+height_list.sort()
+print(height_list)
